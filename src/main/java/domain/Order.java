5ab24fc6b2bc58a0f43bf154df81e8252d73425f
toneyparky <qkrtmddhks95@gmail.com>
2020-03-04 13:56:33 +0900

    feat: Order에 들어온 amount 예외처리

--- src/main/java/domain/Order.java
+++ src/main/java/domain/Order.java
@@ -11,13 +11,24 @@ import java.util.Objects;
  * 날짜 : 2020/03/04
  */
 public class Order {
+	private static final int MIN_AMOUNT = 1;
+	private static final int MAX_AMOUNT = 99;
+
 	private final Menu menu;
 	private int amount;
 
 	public Order(Menu inputMenu, int inputAmount) {
 		Objects.requireNonNull(inputMenu, "메뉴는 null일 수 없습니다.");
+		validateAmount(inputAmount);
 		this.menu = inputMenu;
 		this.amount = inputAmount;
 	}
+
+	private void validateAmount(int inputAmount) {
+		if (inputAmount < MIN_AMOUNT || inputAmount > MAX_AMOUNT) {
+			throw new IllegalArgumentException("1개에서 99개 사이로 주문해야합니다.");
+		}
+	}
+
 
 }
--- src/test/java/domain/OrderTest.java
+++ src/test/java/domain/OrderTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -25,7 +26,7 @@ public class OrderTest {
 
 	@DisplayName("Order 생성자 유효하지 않은 메뉴 입력시 예외처리")
 	@Test
-	void Order_invalid_input() {
+	void Order_invalid_menu_input() {
 		assertThatIllegalArgumentException().isThrownBy(() -> {
 			new Order(MenuRepository.getMenu(88), 1);
 		}).withMessage("해당되는 번호의 메뉴가 없습니다.");
@@ -34,9 +35,18 @@ public class OrderTest {
 	@DisplayName("Order 생성자 null 메뉴 입력시 예외처리")
 	@ParameterizedTest
 	@NullSource
-	void Order_Null_input(Menu nullMenu) {
+	void Order_Null_menu_input(Menu nullMenu) {
 		assertThatNullPointerException().isThrownBy(() -> {
 			new Order(nullMenu, 1);
 		}).withMessage("메뉴는 null일 수 없습니다.");
+	}
+
+	@DisplayName("Order 생성자 유효하지 않은 수량 입력시 예외처리")
+	@ParameterizedTest
+	@ValueSource(ints = {0, 100})
+	void Order_invalid_amount_input(int inputAmount) {
+		assertThatIllegalArgumentException().isThrownBy(() -> {
+			new Order(MenuRepository.getMenu(1), inputAmount);
+		}).withMessage("1개에서 99개 사이로 주문해야합니다.");
 	}
 }
